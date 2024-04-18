53ed006833b86c7b1d5e684def752839708e6722
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-04-18 23:09:06 +0200

    LANDINGPAGE IN PROGRESS

--- bibliotecaIETI/settings.py
+++ bibliotecaIETI/settings.py
@@ -129,10 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 
-#Imagenes y videos
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+# Media Files (Uploaded by Users)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

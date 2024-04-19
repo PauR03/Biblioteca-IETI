584a907ec6eea09dfe93eef2e813822481486347
arnaumv <arnaumestre02@gmail.com>
2024-04-19 17:03:57 +0200

    cambios

--- bibliotecaIETI/settings.py
+++ bibliotecaIETI/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-AUTH_USER_MODEL = 'biblioteca.User'
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'

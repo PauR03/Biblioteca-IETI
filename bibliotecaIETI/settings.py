5d147becfbfb380704e39a53217f7fdca1e7a2da
arnaumv <arnaumestre02@gmail.com>
2024-04-19 16:58:01 +0200

    ..

--- bibliotecaIETI/settings.py
+++ bibliotecaIETI/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-
+AUTH_USER_MODEL = 'biblioteca.User'
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'biblioteca/static'),]
 
 # Media Files (Uploaded by Users)
 MEDIA_URL = '/media/'

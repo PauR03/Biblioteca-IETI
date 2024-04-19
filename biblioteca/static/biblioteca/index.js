1ab5a0bbf2fb550432b01eece04e1b483c6db782
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-04-20 01:24:07 +0200

    index y dashbaord

--- biblioteca/static/biblioteca/index.js
+++ biblioteca/static/biblioteca/index.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
             url: $(this).attr('action'),
             type: $(this).attr('method'),
             data: $(this).serialize(),
+            headers: {
+                'X-CSRFToken': getCookie('csrftoken')  // Include the CSRF token in the request header
+            },
             success: function(data) {
                 if (data.redirect) {
                     // data.redirect contains the string URL to redirect
@@ -23,3 +26,20 @@ $(document).ready(function() {
         });
     });
 });
+
+// Function to get a cookie by name
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.http import JsonResponse
+from django.views.decorators.cache import never_cache
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 from .backends import EmailBackend
 from django.http import JsonResponse
 from django.contrib.auth.models import User
@@ -22,6 +27,7 @@ from django.shortcuts import redirect
 
 from django.http import HttpResponse
 
+@never_cache
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -30,13 +36,11 @@ def login_view(request):
         user = backend.authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            token, created = Token.objects.get_or_create(user=user)
             request.session['username'] = username
-            request.session['token'] = token.key
-            return redirect('dashboard')  # Redirect to the 'dashboard' view
+            return JsonResponse({'redirect': '/dashboard/'})  # Return a JSON response with the URL to redirect to
         else:
-            # manejar el caso de inicio de sesión fallido
-            response = HttpResponse('Invalid login credentials')
+            # handle failed login case
+            response = JsonResponse({'error': 'Invalid login credentials'})
             response.status_code = 401  # Or another appropriate HTTP status code
             return response
     else:
@@ -47,4 +51,3 @@ def dashboard_view(request):
     username = request.session.get('username')
     token = request.session.get('token')
     return render(request, 'dashboard.html', {'username': username, 'token': token})
-
